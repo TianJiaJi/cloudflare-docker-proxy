ab291261156088025a23dff353139880d276c996
additive <100060706+TianJiaJi@users.noreply.github.com>
2024-06-24 14:46:11 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import DOCS from './tips.html';
 
-addEventListener("fetch"， (event) => {
+addEventListener("fetch", (event) => {
   event.passThroughOnException();
   event.respondWith(handleRequest(event.request));
 });
